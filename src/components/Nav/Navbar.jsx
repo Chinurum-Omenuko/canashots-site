f0d4d3d2fcf1130a95ffbbd4fe2e9892d3fdd54e
chinurum-omenuko <omen0003@algonquinlive.com>
2023-09-16 11:35:22 -0400

    makes navbar and adds to layout

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,9 +1,16 @@
+
 export default function Navbar() {
     return (
-        <nav>
-            <h1>Canashots</h1>
-
-            
+        <nav className="navbar navbar-default">
+            <div className="container-fluid flex flex-row justify-between">
+                <a href="/">Canashots</a>
+                
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
         </nav>
     )
 }
